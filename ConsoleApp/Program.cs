a6d3ecd6029fc09fd649e94241005eb8c55558f4
João Felipe Zini <joao.zini@semprocesso.com.br>
2024-06-25 11:44:52 -0300

    TryParse

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,10 +2,28 @@
 {
     public static void Main()
     {
-        long someLongNumber = 51614;
-        // int intNumber = Convert.ToInt32(someLongNumber);
-        int intNumber = (int)someLongNumber;
+        // long someLongNumber = 51614;
+        // // int intNumber = Convert.ToInt32(someLongNumber);
+        // int intNumber = (int)someLongNumber;
 
-        Console.WriteLine(intNumber);
+        // Console.WriteLine(intNumber);
+        // string str = "123";
+        // // int num = Convert.ToInt32(str);
+        // int num = int.Parse(str);
+        // Console.WriteLine(num);
+
+        // const int num = 123;
+        // string str = num.ToString();
+        // Console.WriteLine(str);
+
+        bool canConvert = int.TryParse("123M", out int number);
+        if (canConvert)
+        {
+            Console.WriteLine(number);
+        }
+        else
+        {
+            Console.WriteLine("Conversion failed");
+        }
     }
 }
